1c810b47675511e294b2d17cfce690297febe442
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2024-03-06 23:10:32 -0500

    edit and delete buttons added

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -145,12 +145,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{PlaceActions}
 				</div>
 			</div>
 			<hr />
